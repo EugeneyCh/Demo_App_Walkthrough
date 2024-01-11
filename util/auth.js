2c46bc5f96b72673ebf7f0a8927fe9c5c214ab83
EugeneyCh <eugeney@ua.fm>
2024-01-11 13:47:21 +0200

    Second_Day

--- util/auth.js
+++ util/auth.js
@@ -2,12 +2,23 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyB4lB0fYjNYywGNQzHeogVr-r7c0vWzPXA";
 
-function createUser() {
-  axios.post(
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+}
+
+export async function createUser(email, password) {
+  const responce = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
       email: email,
-      password: pa,
+      password: password,
+      returnSecureToken: true,
     }
   );
 }
